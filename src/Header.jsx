82662960d59be36424ea8e84c8630af9eab310f2
Ambs04 <amberleejones2007@gmail.com>
2025-04-26 12:53:23 +0200

    create header component without styling

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <img
+        src="https://gimgs2.nohat.cc/thumb/f/640/meme-stickers-troll-face--m2H7i8Z5i8H7H7A0.jpg"
+        alt="troll-face-image"
+      />
+      <h2>Meme Generator</h2>
+      <h4>React Course - DJS07</h4>
+    </header>
+  );
+}
