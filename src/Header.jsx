2282ca6920fd32fca4d51199d946c5208604c099
Ambs04 <amberleejones2007@gmail.com>
2025-04-26 12:54:31 +0200

    add classNames to component elements to apply styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header>
+    <header className="header">
       <img
         src="https://gimgs2.nohat.cc/thumb/f/640/meme-stickers-troll-face--m2H7i8Z5i8H7H7A0.jpg"
         alt="troll-face-image"
+        className="header--image"
       />
-      <h2>Meme Generator</h2>
-      <h4>React Course - DJS07</h4>
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - DJS07</h4>
     </header>
   );
 }
